a3622cea4d6999b94b7b1633af39aec35e6a1b95
Vilstig <fj.janas@gmail.com>
2025-03-24 23:18:16 +0100

    created read_log function, handling the input

---
+++ File_reader.py
@@ -0,0 +1,48 @@
+import datetime
+import ipaddress
+import sys
+from sys import dont_write_bytecode
+
+
+def parse_log_line(line):
+    parsed_line = line.strip().split('\t')
+
+    try:
+        ts = float(parsed_line[0])
+        uid = parsed_line[1]
+        id_orig_h = ipaddress.ip_address(parsed_line[2])
+        id_orig_p = int(parsed_line[3])
+        id_resp_h = ipaddress.ip_address(parsed_line[4])
+        id_resp_p = int(parsed_line[5])
+        method = parsed_line[7]
+        host = ipaddress.ip_address(parsed_line[8])
+        uri = parsed_line[9]
+
+        timestamp = datetime.datetime.fromtimestamp(ts)
+
+        return timestamp, uid, id_orig_h, id_orig_p, id_resp_h,id_resp_p, method, host, uri
+
+    except ValueError:
+        return None
+
+
+def read_log(stream):
+    log_entries = []
+
+    for line in stream:
+        line = line.strip()
+        if line:
+            parsed_entry = parse_log_line(line)
+            if parsed_entry:
+                log_entries.append(parsed_entry)
+
+    return log_entries
+
+
+def main():
+    with open('http_first_100k.log', 'r') as log_file:
+        for log_entry in read_log(log_file):
+            print(log_entry)
+
+if __name__ == '__main__':
+    main()
